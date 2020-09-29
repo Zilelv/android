adc4f061ce05abdfe945f1b083030c22b1eacdf9
agarcia <agarcia@solidgeargroup.com>
2020-09-29 14:45:55 +0200

    Revert multidex support

--- owncloudApp/src/main/java/com/owncloud/android/MainApp.kt
+++ owncloudApp/src/main/java/com/owncloud/android/MainApp.kt
@@ -22,6 +22,7 @@
 package com.owncloud.android
 
 import android.app.Activity
+import android.app.Application
 import android.content.Context
 import android.content.pm.PackageInfo
 import android.content.pm.PackageManager
@@ -29,8 +30,6 @@ import android.os.Build
 import android.os.Bundle
 import android.os.Environment
 import android.view.WindowManager
-import androidx.multidex.MultiDex
-import androidx.multidex.MultiDexApplication
 import com.owncloud.android.authentication.BiometricManager
 import com.owncloud.android.authentication.PassCodeManager
 import com.owncloud.android.authentication.PatternManager
@@ -62,7 +61,7 @@ import java.io.File
  * Contains methods to build the "static" strings. These strings were before constants in different
  * classes
  */
-class MainApp : MultiDexApplication() {
+class MainApp : Application() {
     override fun onCreate() {
         super.onCreate()
 
@@ -147,11 +146,6 @@ class MainApp : MultiDexApplication() {
         })
 
         initDependencyInjection()
-    }
-
-    override fun attachBaseContext(base: Context?) {
-        super.attachBaseContext(base)
-        MultiDex.install(this)
     }
 
     fun startLogIfDeveloper() {
